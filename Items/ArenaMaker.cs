564b90c8a4ce1af196e7bd2d5830c4f4b53512b8
LowQualityTrashXinim <LELwhyyoulookingatthis@gmail.com>
2022-11-20 19:51:06 +0700

    Arena maker that i kinda take inspiration from

---
+++ Items/ArenaMaker.cs
@@ -0,0 +1,51 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+
+namespace BossRush.Items
+{
+    internal class ArenaMaker : ModItem
+    {
+        public override string Texture => "BossRush/MissingTexture";
+
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Create a basic arena that 300 block wide" +
+                "\nWill spawn at the middle of the mouse");
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 30;
+            Item.height = 30;
+            
+            Item.useTime = 99;
+            Item.useAnimation = 99;
+
+            Item.rare = ItemRarityID.White;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.noUseGraphic = true;
+            Item.noMelee = true;
+
+            Item.shoot = ModContent.ProjectileType<ArenaMakerProj>();
+            Item.shootSpeed = 0;
+
+            Item.consumable = true;
+            Item.maxStack = 99;
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            position = Main.MouseWorld;
+            Projectile.NewProjectile(source, position, Vector2.Zero, type, 0, 0, player.whoAmI);
+            return false;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.WoodPlatform, 300)
+                .Register();
+        }
+    }
+}
---
+++ Items/ArenaMakerProj.cs
@@ -0,0 +1,55 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace BossRush.Items
+{
+    internal class ArenaMakerProj : ModProjectile
+    {
+        //This should be breaking a lot, i can't bother to make it stable as for now
+        public override string Texture => "BossRush/MissingTexture";
+        public override void SetDefaults()
+        {
+            Projectile.width = 1;
+            Projectile.height = 1;
+            Projectile.timeLeft = 1;
+            Projectile.friendly = true;
+            Projectile.aiStyle = 16;
+            Projectile.penetrate = -1;
+        }
+        public override bool? CanDamage()
+        {
+            return false;
+        }
+        public override void Kill(int timeLeft)
+        {
+            Vector2 position = Projectile.Center;
+            const int length = 150;
+            bool goLeft = Projectile.Center.X < Main.player[Projectile.owner].Center.X;
+            int min = goLeft ? -length : length;
+            int max = goLeft ? length : -length;
+            if (Main.netMode == NetmodeID.MultiplayerClient)
+            {
+                return;
+            }
+            for (int x = min; x <= max; x++)
+            {
+                int xPos = (int)(x + position.X/16.0f);
+                int yPos = (int)(position.Y / 16.0f);
+                if (xPos < 0 || xPos >= Main.maxTilesX || yPos < 0 || yPos >= Main.maxTilesY)
+                    continue;
+
+                Tile tile = Main.tile[xPos, yPos];
+
+                if (tile == null)
+                    continue;
+
+                WorldGen.PlaceTile(xPos, yPos, TileID.Platforms);
+                if (Main.netMode == NetmodeID.Server)
+                    NetMessage.SendTileSquare(-1, xPos, yPos, 1);
+
+            }
+        }
+    }
+}
