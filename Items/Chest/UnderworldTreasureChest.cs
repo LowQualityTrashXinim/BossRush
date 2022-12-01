5a20a0727c44b594e997e704c6f63f255551792c
LowQualityTrashXinim <LELwhyyoulookingatthis@gmail.com>
2022-12-01 22:48:18 +0700

    added new chest

---
+++ Items/Chest/UnderworldTreasureChest.cs
@@ -0,0 +1,47 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+
+namespace BossRush.Items.Chest
+{
+    internal class UnderworldTreasureChest : ChestLootDrop
+    {
+        public override string Texture => "BossRush/Items/Chest/PlaceHolderTreasureChest";
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Good Luck!");
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 54;
+            Item.height = 38;
+            Item.rare = 0;
+        }
+
+        public override bool CanRightClick()
+        {
+            return true;
+        }
+        public override List<int> FlagNumber()
+        {
+            return new List<int> { 0 };
+        }
+        public override void RightClick(Player player)
+        {
+            var entitySource = player.GetSource_OpenItem(Type);
+            GetAmount(out int weaponAmount, out int potionTypeAmount, out int potionNum, player);
+            for (int i = 0; i < weaponAmount; i++)
+            {
+                GetWeapon(player, out int weapon, out int specialAmount);
+                player.QuickSpawnItem(entitySource, weapon, specialAmount);
+            }
+            for (int i = 0; i < potionTypeAmount; i++)
+            {
+                GetPotion(out int potion);
+                player.QuickSpawnItem(entitySource, potion, potionNum);
+            }
+        }
+    }
+
+}
