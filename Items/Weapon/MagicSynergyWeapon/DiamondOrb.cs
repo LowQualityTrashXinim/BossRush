f7be3c32db289945208d91e39ff9b7690c45e37f
LowQualityTrashXinim <LELwhyyoulookingatthis@gmail.com>
2023-01-06 21:29:26 +0700

    small re-organize

--- Items/Weapon/MagicSynergyWeapon/DiamondOrb.cs
+++ Items/Weapon/MagicSynergyWeapon/DiamondOrb.cs
@@ -67,4 +67,46 @@ namespace BossRush.Items.Weapon.MagicSynergyWeapon
             }
         }
     }
+    internal class DiamondBoltSpecial : ModProjectile
+    {
+        public override string Texture => "BossRush/Items/Weapon/MagicSynergyWeapon/DiamondOrb";
+        public override void SetDefaults()
+        {
+            Projectile.width = 30;
+            Projectile.height = 30;
+            Projectile.timeLeft = 70;
+            Projectile.tileCollide = false;
+            Projectile.penetrate = -1;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.alpha = 255;
+        }
+        int count = 0;
+        public override void AI()
+        {
+            if (Projectile.timeLeft > 55)
+            {
+                Projectile.alpha -= 10;
+            }
+            else
+            {
+                Projectile.alpha += 5;
+                Projectile.scale -= 0.025f;
+            }
+            if (count >= 3)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    int dustnumber = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GemDiamond, 0, 0, 100, default, Projectile.scale);
+                    Main.dust[dustnumber].noGravity = true;
+                }
+                count = 0;
+            }
+            count++;
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            target.immune[Projectile.owner] = 1;
+        }
+    }
 }
--- Items/Weapon/MagicSynergyWeapon/GiantRubyBolt.cs
+++ Items/Weapon/MagicSynergyWeapon/GiantRubyBolt.cs
@@ -95,4 +95,37 @@ namespace BossRush.Items.Weapon.MagicSynergyWeapon
             }
         }
     }
+    internal class SmallerRubyBolt : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 30;
+            Projectile.height = 30;
+            Projectile.timeLeft = 50;
+            Projectile.tileCollide = false;
+            Projectile.penetrate = -1;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Magic;
+        }
+        int count = 0;
+        public override void AI()
+        {
+            Projectile.alpha += 5;
+            Projectile.scale -= 0.015f;
+            if (count >= 3)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    int dustnumber = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GemRuby, -Projectile.velocity.X + Main.rand.Next(-5, 5), -Projectile.velocity.Y + Main.rand.Next(-5, 5), 100, default, Projectile.scale);
+                    Main.dust[dustnumber].noGravity = true;
+                }
+                count = 0;
+            }
+            count++;
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            target.immune[Projectile.owner] = 1;
+        }
+    }
 }
