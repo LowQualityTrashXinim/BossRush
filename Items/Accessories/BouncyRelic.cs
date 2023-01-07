a94ccf22859d7318089d261a65b06949af46bee4
LowQualityTrashXinim <LELwhyyoulookingatthis@gmail.com>
2023-01-07 21:43:15 +0700

    Change name and fix some small grammar

--- Items/Accessories/BouncyRelic.cs
+++ Items/Accessories/BouncyRelic.cs
@@ -15,7 +15,7 @@ namespace BossRush.Items.Accessories
         {
             Tooltip.SetDefault("\"A small ancient power accidentally left from a old god, said to be cursed because of jealousy from the new god\"" +
                 "\nMost projectile can bounce of tile" +
-                "\nBouncing off a tile increase damage by 20 %" +
+                "\nBouncing off a tile increase damage by 20%" +
                 "\nBouncing off a tile will make the projectile disappear after 3s");
         }
         public override void SetDefaults()
