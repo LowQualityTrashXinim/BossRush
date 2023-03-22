d48123959ac8abd0abb4b0040a319bd5208bfa74
Xinim <LELwhyyoulookingatthis@gmail.com>
2023-03-22 23:39:31 +0700

    Update EnhancedKatana

--- Items/Weapon/MeleeSynergyWeapon/EnchantedSilverSwordP.cs
+++ Items/Weapon/MeleeSynergyWeapon/EnchantedSilverSwordP.cs
@@ -2,8 +2,6 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Terraria.GameContent;
 
 namespace BossRush.Items.Weapon.MeleeSynergyWeapon
 {
