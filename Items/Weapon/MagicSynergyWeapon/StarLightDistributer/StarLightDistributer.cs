728be4c41c76b35d14f065bca1cb01ddee5bbf77
Xinim <LELwhyyoulookingatthis@gmail.com>
2023-04-03 08:48:52 +0700

    move files to folder

--- Items/Weapon/MagicSynergyWeapon/StarLightDistributer/StarLightDistributer.cs
+++ Items/Weapon/MagicSynergyWeapon/StarLightDistributer/StarLightDistributer.cs
@@ -5,7 +5,7 @@ using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace BossRush.Items.Weapon.MagicSynergyWeapon
+namespace BossRush.Items.Weapon.MagicSynergyWeapon.StarLightDistributer
 {
     internal class StarLightDistributer : ModItem, ISynergyItem
     {
