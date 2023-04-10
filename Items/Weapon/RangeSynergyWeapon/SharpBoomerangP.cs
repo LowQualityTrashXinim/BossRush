e290a48a8ab4036caf7a9cf13447414bd59904f6
Xinim <LELwhyyoulookingatthis@gmail.com>
2023-04-10 10:23:07 +0700

    Update sharp boomerang, rename interface file

--- Items/Weapon/RangeSynergyWeapon/SharpBoomerangP.cs
+++ Items/Weapon/RangeSynergyWeapon/SharpBoomerangP.cs
@@ -1,10 +1,8 @@
-﻿using Terraria;
+﻿using System;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Terraria.GameContent;
-using System;
-using Terraria.ID;
 
 namespace BossRush.Items.Weapon.RangeSynergyWeapon
 {
@@ -17,43 +15,50 @@ namespace BossRush.Items.Weapon.RangeSynergyWeapon
             Projectile.scale = 0.75f;
             Projectile.width = 38;
             Projectile.height = 72;
-            Projectile.timeLeft = 23;
-            Projectile.DamageType = DamageClass.Ranged;
             Projectile.friendly = true;
             Projectile.penetrate = -1;
-            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 5;
-            ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+            Projectile.DamageType = DamageClass.Ranged;
         }
         int count = 0;
-
         public override void AI()
         {
             Player player = Main.player[Projectile.owner];
-            float distance = 60;
-            float distanceTo = Vector2.Distance(player.Center, Projectile.Center);
-            Projectile.rotation += MathHelper.ToRadians(60);
-            if (Projectile.timeLeft < 3 || distanceTo > 200)
-            {
-                Vector2 GoBack = player.Center - Projectile.Center;
-                Vector2 SafeGoBack = GoBack.SafeNormalize(Vector2.UnitY);
-                if (count >= 15)
-                {
-                    Projectile.knockBack = 0;
-                    Projectile.velocity = SafeGoBack * 40f;
-                }
-                else
-                {
-                    Projectile.velocity += SafeGoBack * 5f;
-                }
-                Projectile.timeLeft = 2;
-                Projectile.tileCollide = false;
+            float MaxLengthX = (Main.MouseWorld - player.Center).Length();
+            float MaxLengthY = MaxLengthX / 3f;
+            Projectile.velocity = Vector2.Normalize(Projectile.velocity);
 
-                if (distanceTo < distance || distanceTo > 1750)
-                {
-                    Projectile.Kill();
-                }
-                count++;
+            int duration = player.itemAnimationMax;
+            if (Projectile.timeLeft > duration)
+            {
+                Projectile.timeLeft = duration;
             }
+            float HalfDuration = duration * 0.5f;
+
+            float progressX;
+            if (Projectile.timeLeft < HalfDuration)
+            {
+                progressX = Projectile.timeLeft / HalfDuration;
+            }
+            else
+            {
+                progressX = (duration - Projectile.timeLeft) / HalfDuration;
+            }
+            float FirstProgress = player.itemAnimationMax / 3f;
+            float SecondProgress = FirstProgress * 2f;
+            float progressY = player.itemAnimation / FirstProgress;
+            if (player.itemAnimation >= FirstProgress)
+            {
+                progressY -= (progressY - 1) * 3f;
+            }
+            if (player.itemAnimation >= SecondProgress)
+            {
+                progressY -= (Math.Abs(progressY) - 2) * 2;
+            }
+            progressY = Math.Clamp(progressY, -1, 1);
+            Vector2 VelocityPosition = new Vector2(MathHelper.SmoothStep(Projectile.velocity.X * 0, Projectile.velocity.X * MaxLengthX, progressX),
+                                                   MathHelper.SmoothStep(Projectile.velocity.Y * 0, Projectile.velocity.Y * MaxLengthY, progressY));
+            Projectile.Center = player.MountedCenter + VelocityPosition;
+            Projectile.rotation += MathHelper.ToRadians(60);
         }
 
         public override bool OnTileCollide(Vector2 oldVelocity)
@@ -66,7 +71,7 @@ namespace BossRush.Items.Weapon.RangeSynergyWeapon
             Vector2 RandomPos = Projectile.Center + Main.rand.NextVector2CircularEdge(50, 50);
             Vector2 DistanceToAim = (target.Center - RandomPos).SafeNormalize(Vector2.UnitX) * 4f;
             Projectile.NewProjectile(Projectile.GetSource_FromThis(), RandomPos, DistanceToAim, ProjectileID.SuperStarSlash, Projectile.damage, 0, Projectile.owner);
-            target.immune[Projectile.owner] = 1;
+            target.immune[Projectile.owner] = 7;
         }
     }
 }
