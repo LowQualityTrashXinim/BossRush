50eeaecc4f63ef4cf3acd2392ec829cc0a9c3c35
Xinim <LELwhyyoulookingatthis@gmail.com>
2023-04-23 17:52:47 +0700

    Update YinYang

--- Contents/Items/Weapon/MeleeSynergyWeapon/YinYang/YinYangBoltProjectile.cs
+++ Contents/Items/Weapon/MeleeSynergyWeapon/YinYang/YinYangBoltProjectile.cs
@@ -1,65 +1,16 @@
 ﻿using Terraria;
+using Terraria.ID;
 using Microsoft.Xna.Framework;
 
 namespace BossRush.Contents.Items.Weapon.MeleeSynergyWeapon.YinYang
 {
     internal class YinLight : BaseBoltProjectile
     {
-        Player player;
-        int Counter = 0;
-        int firstframe = 0;
-        const float FullCircle = MathHelper.TwoPi * 100;
-        public override bool PreAI()
-        {
-            if (firstframe == 0)
-            {
-                player = Main.player[Projectile.owner];
-                firstframe++;
-            }
-            return true;
-        }
-        public override void AI()
-        {
-            if (player.dead || !player.active)
-            {
-                Projectile.Kill();
-            }
-            if (Counter >= FullCircle)
-            {
-                Counter = 0;
-            }
-            Counter++;
-            Projectile.Center = getPosToReturn(0, Counter);
-        }
+        public override int Offset() => 0;
     }
 
     internal class YangDark : BaseBoltProjectile
     {
-        Player player;
-        int Counter = 0;
-        int firstframe = 0;
-        const float FullCircle = MathHelper.TwoPi * 100;
-        public override bool PreAI()
-        {
-            if (firstframe == 0)
-            {
-                player = Main.player[Projectile.owner];
-                firstframe++;
-            }
-            return true;
-        }
-        public override void AI()
-        {
-            if (player.dead || !player.active)
-            {
-                Projectile.Kill();
-            }
-            if (Counter >= FullCircle)
-            {
-                Counter = 0;
-            }
-            Counter++;
-            Projectile.Center = getPosToReturn(180, Counter);
-        }
+        public override int Offset() => 180;
     }
 }
--- Contents/Items/Weapon/MeleeSynergyWeapon/YinYang/YinYangP.cs
+++ Contents/Items/Weapon/MeleeSynergyWeapon/YinYang/YinYangP.cs
@@ -15,7 +15,7 @@ namespace BossRush.Contents.Items.Weapon.MeleeSynergyWeapon.YinYang
         public override void SetDefaults()
         {
             ProjectileID.Sets.YoyosMaximumRange[Projectile.type] = 400f;
-            ProjectileID.Sets.YoyosTopSpeed[Projectile.type] = 12;
+            ProjectileID.Sets.YoyosTopSpeed[Projectile.type] = 15;
             Projectile.friendly = true;
             Projectile.penetrate = -1;
             Projectile.aiStyle = 99;
@@ -23,83 +23,84 @@ namespace BossRush.Contents.Items.Weapon.MeleeSynergyWeapon.YinYang
             Projectile.width = 24;
             Projectile.height = 24;
         }
-        Player player;
-        int count = 0;
         int charge = 0;
-        public override void AI()
-        {
-            if (count == 0)
-            {
-                player = Main.player[Projectile.owner];
-                Projectile.ai[1] = player.ownedProjectileCounts[Projectile.type];
-                count++;
-            }
-        }
+        int spawnCounter = 5;
 
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
             charge++;
+            Player player = Main.player[Projectile.owner];
             if (charge == 17 && player.yoyoGlove == false)
             {
-                Projectile.NewProjectile(Projectile.GetSource_FromAI(), Projectile.position, Vector2.Zero, ModContent.ProjectileType<YinYangShockWave>(), 0, 0, player.whoAmI);
+                Projectile.NewProjectile(Projectile.GetSource_FromAI(), Projectile.position, Vector2.Zero, ModContent.ProjectileType<YinYangShockWave>(), 0, 0, Projectile.owner);
                 Projectile.damage = (int)(Projectile.damage * 1.75f);
                 ProjectileID.Sets.YoyosTopSpeed[Projectile.type] = 24;
                 ProjectileID.Sets.YoyosMaximumRange[Projectile.type] = 600f;
-                Projectile.NewProjectile(Projectile.GetSource_FromAI(), Projectile.position, Vector2.Zero, ModContent.ProjectileType<YinLight>(), (int)(Projectile.damage * 0.75f), 2f, player.whoAmI, Projectile.ai[1]);
-                Projectile.NewProjectile(Projectile.GetSource_FromAI(), Projectile.position, Vector2.Zero, ModContent.ProjectileType<YangDark>(), (int)(Projectile.damage * 0.75f), 2f, player.whoAmI, Projectile.ai[1]);
             }
-            if (charge > 17 || player.yoyoGlove == true)
+            if (charge >= 17 || player.yoyoGlove == true)
             {
-                Vector2 RotatePos = Main.rand.NextVector2CircularEdge(75f, 75f) * 5 + Projectile.position;
-                Vector2 Aimto = (target.Center - RotatePos).SafeNormalize(Vector2.UnitX) * 3;
-                Projectile.NewProjectile(Projectile.GetSource_FromAI(), RotatePos, Aimto, ModContent.ProjectileType<YinLight>(), (int)(Projectile.damage * 0.75f), 2f, player.whoAmI);
-                RotatePos = Main.rand.NextVector2CircularEdge(75f, 75f) * 5 + Projectile.position;
-                Aimto = (target.Center - RotatePos).SafeNormalize(Vector2.UnitX) * 3;
-                Projectile.NewProjectile(Projectile.GetSource_FromAI(), RotatePos, Aimto, ModContent.ProjectileType<YangDark>(), (int)(Projectile.damage * 0.75f), 2f, player.whoAmI);
+                //Vector2 RotatePos = Main.rand.NextVector2CircularEdge(75f, 75f) * 5 + Projectile.position;
+                //Vector2 Aimto = (target.Center - RotatePos).SafeNormalize(Vector2.UnitX) * 3;
+                //Projectile.NewProjectile(Projectile.GetSource_FromAI(), RotatePos, Aimto, ModContent.ProjectileType<YinLight>(), (int)(Projectile.damage * 0.75f), 2f, Projectile.owner, 0);
+                //RotatePos = Main.rand.NextVector2CircularEdge(75f, 75f) * 5 + Projectile.position;
+                //Aimto = (target.Center - RotatePos).SafeNormalize(Vector2.UnitX) * 3;
+                //Projectile.NewProjectile(Projectile.GetSource_FromAI(), RotatePos, Aimto, ModContent.ProjectileType<YangDark>(), (int)(Projectile.damage * 0.75f), 2f, Projectile.owner, 0);
+                if (spawnCounter >= 5)
+                {
+                    Projectile.NewProjectile(Projectile.GetSource_FromAI(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<YinLight>(), Projectile.damage, 0, Projectile.owner, 1);
+                    Projectile.NewProjectile(Projectile.GetSource_FromAI(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<YangDark>(), Projectile.damage, 0, Projectile.owner, 1);
+                    spawnCounter = 0;
+                }
+                spawnCounter++;
             }
         }
     }
 
     abstract class BaseBoltProjectile : ModProjectile
     {
-        protected Projectile GetYoyoMainProjectile()
-        {
-            for (int i = 0; i < Main.maxProjectiles; i++)
-            {
-                if (Main.projectile[i].ModProjectile is YinYangP && Main.projectile[i].ai[1] == Projectile.ai[0])
-                {
-                    return Main.projectile[i];
-                }
-            }
-            return null;
-        }
-        public Vector2 getPosToReturn(float offSet, int Counter, float Distance = 50)
-        {
-            Vector2 Rotate = new Vector2(1, 1).RotatedBy(MathHelper.ToRadians(offSet));
-            Projectile proj = GetYoyoMainProjectile();
-            Vector2 ProjectileMainCenter = proj != null ? proj.Center : Vector2.Zero;
-            return ProjectileMainCenter + Rotate.RotatedBy(Counter * 0.05f) * Distance;
-        }
         public override void SetDefaults()
         {
             Projectile.DamageType = DamageClass.Melee;
             Projectile.width = 10;
             Projectile.height = 10;
+            Projectile.light = 1f;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 999;
             Projectile.friendly = true;
-            Projectile.penetrate = 1;
-            Projectile.timeLeft = 200;
-            Projectile.tileCollide = false;
             Projectile.extraUpdates = 6;
+            Projectile.tileCollide = false;
             ProjectileID.Sets.TrailCacheLength[Projectile.type] = 50;
             ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
-            Projectile.light = 1f;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 20;
         }
-
-        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        public virtual int Offset() => 0;
+        int Counter = 0;
+        int frame = 0;
+        int distance = 1;
+        Vector2 projCenter;
+        public override void AI()
         {
-            target.immune[Projectile.owner] = 2;
-        }
+            if (Projectile.ai[0] == 0)
+            {
+                Projectile.penetrate = 1;
+                return;
+            }
+            if (frame == 0)
+            {
+                projCenter = Projectile.Center;
+            }
+            distance += frame % 6 == 0 ? 1 : 0;
+            frame++;
+            Counter++;
 
+            Projectile.Center = getPosToReturn(Offset(), Counter, projCenter, distance);
+        }
+        public Vector2 getPosToReturn(float offSet, int Counter, Vector2 pos, float Distance = 50)
+        {
+            Vector2 Rotate = new Vector2(1, 1).RotatedBy(MathHelper.ToRadians(offSet));
+            return pos + Rotate.RotatedBy(MathHelper.ToRadians(Counter)) * Distance;
+        }
         public override bool PreDraw(ref Color lightColor)
         {
             Projectile.DrawTrail(lightColor, .02f);
