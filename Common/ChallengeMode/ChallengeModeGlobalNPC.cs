5d51c7e4e9091f71a2569ede5fc937ea7787f8bb
Xinim <LELwhyyoulookingatthis@gmail.com>
2023-05-30 10:55:49 +0700

    nurse are insta kill, no fun allow :>

--- Common/ChallengeMode/ChallengeModeGlobalNPC.cs
+++ Common/ChallengeMode/ChallengeModeGlobalNPC.cs
@@ -1,10 +1,19 @@
 ﻿using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace BossRush.Common.ChallengeMode
 {
     internal class ChallengeModeGlobalNPC : GlobalNPC
     {
+        public override void OnSpawn(NPC npc, IEntitySource source)
+        {
+            if(npc.type == NPCID.Nurse)
+            {
+                npc.StrikeInstantKill();
+            }
+        }
         public override void ModifyShop(NPCShop shop)
         {
             if (ModContent.GetInstance<BossRushModConfig>().EnableChallengeMode)
