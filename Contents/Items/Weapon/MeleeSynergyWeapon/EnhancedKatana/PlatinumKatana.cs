75a96fe13fbfe9b94af2c8645f33c03e72feb1b0
Xinim <LELwhyyoulookingatthis@gmail.com>
2023-06-03 20:02:10 +0700

    Update sprite but accidentally shrink it, shit

--- Contents/Items/Weapon/MeleeSynergyWeapon/EnhancedKatana/PlatinumKatana.cs
+++ Contents/Items/Weapon/MeleeSynergyWeapon/EnhancedKatana/PlatinumKatana.cs
@@ -37,21 +37,31 @@ namespace BossRush.Contents.Items.Weapon.MeleeSynergyWeapon.EnhancedKatana
     {
         protected virtual float HoldoutRangeMax => 50f;
         protected virtual float HoldoutRangeMin => 10f;
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = 8;
+        }
         public override void SetDefaults()
         {
             Projectile.DamageType = DamageClass.Melee;
-            Projectile.width = Projectile.height = 88;
+            Projectile.width = 40; Projectile.height = 54;
             Projectile.penetrate = -1;
             Projectile.light = 0.1f;
             Projectile.tileCollide = false;
             Projectile.friendly = true;
             Projectile.wet = false;
-
-            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 10;
-            ProjectileID.Sets.TrailingMode[Projectile.type] = 3;
+        }
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            Vector2 Direction = (Projectile.rotation + MathHelper.ToRadians(90)).ToRotationVector2() * 60;
+            Vector2 Head = Projectile.Center + Direction;
+            Vector2 End = Projectile.Center - Direction;
+            float point = 0f;
+            return Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), Head, End, Projectile.width, ref point);
         }
         public override void AI()
         {
+            SelectFrame();
             Projectile.ai[1]++;
             Player player = Main.player[Projectile.owner];
             player.heldProj = Projectile.whoAmI;
@@ -61,6 +71,14 @@ namespace BossRush.Contents.Items.Weapon.MeleeSynergyWeapon.EnhancedKatana
                 Projectile.timeLeft = duration;
             }
             Projectile.rotation = Projectile.velocity.ToRotation();
+            if (Projectile.ai[0] % 2 == 0)
+            {
+                if (Projectile.timeLeft == duration)
+                {
+                    Projectile.spriteDirection = -Projectile.spriteDirection;
+                }
+                Projectile.rotation += MathHelper.ToRadians(180);
+            }
             Projectile.velocity = Vector2.Normalize(Projectile.velocity);
             float halfDuration = duration * .5f;
             float progress = (duration - Projectile.timeLeft) / halfDuration;
@@ -71,21 +89,31 @@ namespace BossRush.Contents.Items.Weapon.MeleeSynergyWeapon.EnhancedKatana
                 Projectile.alpha += 20;
             }
         }
-
+        public void SelectFrame()
+        {
+            if (++Projectile.frameCounter >= 1)
+            {
+                Projectile.frameCounter = 0;
+                Projectile.frame += 1;
+                if (Projectile.frame >= Main.projFrames[Projectile.type])
+                {
+                    Projectile.frame = Main.projFrames[Projectile.type] - 1;
+                }
+            }
+        }
         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
         {
             target.immune[Projectile.owner] = 8;
         }
-
-        public override bool PreDraw(ref Color lightColor)
-        {
-            Main.instance.LoadProjectile(Projectile.type);
-            Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
-            Vector2 origin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
-            Vector2 drawPos = Projectile.position - Main.screenPosition + origin + new Vector2(0f, Projectile.gfxOffY);
-            SpriteEffects spriteEffects = Projectile.ai[0] % 2 == 0 ? SpriteEffects.None : SpriteEffects.FlipVertically;
-            Main.EntitySpriteDraw(texture, drawPos, null, lightColor, Projectile.rotation, origin, Projectile.scale, spriteEffects, 0);
-            return false;
-        }
+        //public override bool PreDraw(ref Color lightColor)
+        //{
+        //    Main.instance.LoadProjectile(Projectile.type);
+        //    Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+        //    Vector2 origin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+        //    Vector2 drawPos = Projectile.position - Main.screenPosition + origin + new Vector2(0f, Projectile.gfxOffY);
+        //    SpriteEffects spriteEffects = Projectile.ai[0] % 2 == 0 ? SpriteEffects.None : SpriteEffects.FlipVertically;
+        //    Main.EntitySpriteDraw(texture, drawPos, null, lightColor, Projectile.rotation, origin, Projectile.scale, spriteEffects, 0);
+        //    return false;
+        //}
     }
 }
