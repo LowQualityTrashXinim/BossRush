da41dc1866574eb91676c9723dd327840763417b
Xinim <LELwhyyoulookingatthis@gmail.com>
2023-06-24 01:08:38 +0700

    achievement data prepare

--- BossRush.cs
+++ BossRush.cs
@@ -1,5 +1,8 @@
 using BossRush.Contents.Items.Card;
 using BossRush.Contents.Items.Chest;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
 using Terraria;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
@@ -9,13 +12,60 @@ namespace BossRush
 {
     public partial class BossRush : Mod
     {
+
+        public Dictionary<int,BossRushAchivement> achievementData;
         public override void Load()
         {
             base.Load();
+            achievementData.Add(1,new BossRushAchivement()
+                {
+                    Name = "The beginning of endless",
+                    Description = "This mark the beginning of where it all start",
+                    ConditionText = "Open your first chest",
+                });
+            achievementData.Add(2, new BossRushAchivement()
+            {
+                Name = "The first artifact holder",
+                Description = "Thing about to get spicy",
+                ConditionText = "Use the first artifact (beside Broken artifact)",
+            });
+            achievementData.Add(3, new BossRushAchivement()
+            {
+                Name = "The first of many",
+                Description = "First time is always the best",
+                ConditionText = "Kill king slime boss",
+            });
+            string path = "C:\\Users\\DELL\\Documents\\My Games\\Terraria\\tModLoader\\BossRushAchievement\\AchievementData.json";
+            CheckIfFileExist(path);
+            using (StreamReader r = new StreamReader(path))
+            {
+
+            }
         }
         public override void Unload()
         {
             base.Unload();
+        }
+        private void CheckIfFileExist(string path)
+        {
+            if (File.Exists(path))
+            {
+                if(!string.IsNullOrEmpty(File.ReadAllText(path)))
+                {
+                    return;
+                }
+                using (StreamWriter sw = File.AppendText(path))
+                {
+
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+
+                }
+            }
         }
     }
     public class BossRushModSystem : ModSystem
@@ -34,7 +84,8 @@ namespace BossRush
     /// </summary>
     public class BossRushAchivement : BossRushCondition, AchievementDataHolder
     {
-        string Name, Description, ConditionText;
+        public string Name, Description, ConditionText;
+        public bool ConditionMet = false;
 
         public AchievementHolder dataholder => Main.LocalPlayer.GetModPlayer<AchievementHolder>();
 
@@ -42,15 +93,16 @@ namespace BossRush
         {
 
         }
-        public BossRushAchivement(string name, string description, string conditionText)
+        public BossRushAchivement(string name, string description, string conditionText, bool ConditionMet)
         {
             Name = name;
             Description = description;
             ConditionText = conditionText;
+            this.ConditionMet = ConditionMet;
         }
         protected int HowManyChestHasPlayerOpenInCurrentSection()
         {
-            if(Main.netMode == NetmodeID.SinglePlayer)
+            if (Main.netMode == NetmodeID.SinglePlayer)
             {
                 return dataholder.chestplayer.CurrentSectionAmountOfChestOpen;
             }
@@ -61,10 +113,6 @@ namespace BossRush
     public class AchievementHolder : ModPlayer
     {
         public ChestLootDropPlayer chestplayer => Main.LocalPlayer.GetModPlayer<ChestLootDropPlayer>();
-        public override bool HoverSlot(Item[] inventory, int context, int slot)
-        {
-            return base.HoverSlot(inventory, context, slot);
-        }
     }
     public interface BossRushCondition
     {
