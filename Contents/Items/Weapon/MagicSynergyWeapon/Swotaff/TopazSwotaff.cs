dc27ef1e3c821f273b947ff03f0c8af6f7fb7824
Xinim <LELwhyyoulookingatthis@gmail.com>
2023-07-06 23:41:53 +0700

    small dmg fix

--- Contents/Items/Weapon/MagicSynergyWeapon/Swotaff/TopazSwotaff.cs
+++ Contents/Items/Weapon/MagicSynergyWeapon/Swotaff/TopazSwotaff.cs
@@ -9,7 +9,7 @@ namespace BossRush.Contents.Items.Weapon.MagicSynergyWeapon.Swotaff
     {
         public override void PreSetDefaults(out int damage, out int ProjectileType, out int ShootType)
         {
-            damage = 17;
+            damage = 15;
             ProjectileType = ModContent.ProjectileType<TopazSwotaffProjectile>();
             ShootType = ProjectileID.TopazBolt;
         }
