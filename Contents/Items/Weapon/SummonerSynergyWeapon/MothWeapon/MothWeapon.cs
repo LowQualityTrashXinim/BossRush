3ee5d35f51da9cd2f7ed668925cd9512b1fcea0a
Sorrow <115921942+rrrrr22@users.noreply.github.com>
2023-07-31 20:27:42 +0300

    Add files via upload

---
+++ Contents/Items/Weapon/SummonerSynergyWeapon/MothWeapon/MothWeapon.cs
@@ -0,0 +1,575 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using BossRush.Contents.Items.Weapon.SummonerSynergyWeapon.StickySlime;
+using BossRush.Texture;
+using rail;
+using static Humanizer.In;
+using System.Drawing.Text;
+using System.Collections.Specialized;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+using System.Collections.Generic;
+
+namespace BossRush.Contents.Items.Weapon.SummonerSynergyWeapon.MothWeapon
+{
+    public class StreetLamp : SynergyModItem
+    {
+
+
+        int lampY = -64;
+
+        public override void SetDefaults()
+        {
+            Item.BossRushSetDefault(128, 128, 28, 5, 32, 32, ItemUseStyleID.HoldUp, true);
+            Item.DamageType = DamageClass.Summon;
+            Item.shoot = ModContent.ProjectileType<MothProj>();
+            Item.mana = 15;
+            Item.noMelee = true;
+            Item.holdStyle = ItemHoldStyleID.HoldUp;
+            Item.color= Color.White;
+            
+
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.White;
+        }
+        public override void HoldSynergyItem(Player player, PlayerSynergyItemHandle modplayer)
+        {
+            base.HoldSynergyItem(player, modplayer);
+
+
+            //add light 
+            Lighting.AddLight(player.Center + new Vector2(0, lampY), 1f, 1f, 1f);
+            Lighting.AddLight(player.Center + new Vector2(0, lampY / 2), 1f, 1f, 0f);
+
+            if (player.HasItem(ItemID.VampireFrogStaff))
+            {
+                modplayer.StreetLamp_VampireFrogStaff = true;
+            }
+            if (player.HasItem(ItemID.FireWhip))
+            {
+                modplayer.StreetLamp_Firecracker = true;
+            }
+            
+        }
+
+        public override void UseStyle(Player player, Rectangle heldItemFrame)
+        {
+            LampHold(player);
+        }
+        public override void HoldStyle(Player player, Rectangle heldItemFrame)
+        {
+            LampHold(player);
+        }
+
+        public override void PostUpdate()
+        {
+            Lighting.AddLight(Item.Center + new Vector2(0, lampY), 1f, 1f, 1f);
+            Lighting.AddLight(Item.Center + new Vector2(0, lampY / 2), 1f, 1f, 0f);
+        }
+
+        // custom hold and use style to fit the theme :D
+        private void LampHold(Player player)
+        {
+
+            player.itemRotation = MathHelper.ToRadians(45 * -player.direction);
+            player.itemLocation = player.Center + new Vector2(8 * player.direction, 16);
+
+
+        }
+
+        public override void ModifySynergyToolTips(ref List<TooltipLine> tooltips, PlayerSynergyItemHandle modplayer)
+        {
+            if (modplayer.StreetLamp_VampireFrogStaff)
+            {
+                tooltips.Add(new TooltipLine(Mod, "StreetLamp_VampireFrogStaff", $"[i:{ItemID.VampireFrogStaff}] Moth's Dash Speed Increased By 15%, also every 3 successful moth dash hits, heal the player by 2.5% of dash attack's bonus damage dealt (affected by vanilla's life steal cooldown mechanic)"));
+            }
+            if (modplayer.StreetLamp_Firecracker)
+            {
+                tooltips.Add(new TooltipLine(Mod, "StreetLamp_Firecracker", $"[i:{ItemID.FireWhip}] Moth's Dash attacks have 10% chance to deal 2x damage and inflict Hellfire for 5 seconds"));
+            }
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            //modify the moths spawn location to be at the lamp
+            position = player.Center + new Vector2(0, lampY);
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+
+            player.AddBuff(ModContent.BuffType<MothBuff>(), 1000);
+
+            var proj = Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, Main.myPlayer);
+            proj.originalDamage = Item.damage;
+            proj.CritChance = Item.crit;
+
+            return false;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.FlinxStaff)
+                .AddIngredient(ItemID.HornetStaff)
+                .AddIngredient(ModContent.ItemType<SynergyEnergy>())
+                .Register();
+        }
+
+    }
+
+    internal class MothBuff : ModBuff
+    {
+        public override string Texture => BossRushTexture.EMPTYBUFF;
+
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = true;
+
+
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            if (player.ownedProjectileCounts[ModContent.ProjectileType<MothProj>()] > 0)
+            {
+
+                player.buffTime[buffIndex] = 180000;
+
+
+            }
+            else
+            {
+
+
+                player.DelBuff(buffIndex);
+                buffIndex--;
+
+            }
+        }
+
+    }
+
+    internal class MothProj : SynergyModProjectile
+    {
+
+        public override void SetStaticDefaults()
+        {
+
+            Main.projFrames[Projectile.type] = 4;
+            ProjectileID.Sets.MinionTargettingFeature[Projectile.type] = true;
+            Main.projPet[Projectile.type] = true;
+            ProjectileID.Sets.MinionSacrificable[Projectile.type] = true;
+            
+
+        }
+
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 48;
+            Projectile.height = 24;
+            Projectile.tileCollide = false;
+            Projectile.friendly = true;
+            Projectile.minionSlots = 1;
+            Projectile.penetrate = -1;
+            Projectile.minion = true;
+            Projectile.usesLocalNPCImmunity= true;
+            Projectile.localNPCHitCooldown = 30;
+        }
+
+        public override bool? CanCutTiles()
+        {
+            return false;
+        }
+
+        public override bool MinionContactDamage()
+        {
+            return true;
+        }
+
+        public override void OnSpawn(IEntitySource source)
+        {
+            Main.NewText("<Moth> LAMP", Color.Beige);
+        }
+
+        public override void SynergyAI(Player player, PlayerSynergyItemHandle modplayer)
+        {
+            //Player player = Main.player[Projectile.owner];
+            if (!CheckActive(player))
+            {
+                return;
+            }
+            //get all the important informations about the target
+            getInfos(player, out Vector2 vectorToIdlePosition, out float distanceToIdlePosition, out bool foundTarget, out float distanceFromTarget, out Vector2 targetCenter);
+            
+            //the movement, attacks, etc... is here
+            behavior(player, vectorToIdlePosition, distanceToIdlePosition, foundTarget, distanceFromTarget, targetCenter, modplayer);
+
+            SelectFrame();
+
+
+            //sprite juice
+            if (currentState == State.dashing)
+            {
+                Projectile.rotation = Projectile.spriteDirection == 1 ? Projectile.velocity.ToRotation() : Projectile.velocity.ToRotation() - MathHelper.ToRadians(180f);
+                
+            }
+            else
+                Projectile.rotation = Projectile.velocity.X * 0.05f;
+            if(currentState != State.windup)
+                Projectile.direction = Projectile.spriteDirection = Projectile.velocity.X > 0f ? 1 : -1;
+            else
+                Projectile.direction = Projectile.spriteDirection = targetCenter.X > Projectile.Center.X ? 1 : -1;
+
+        }
+
+        public override void OnHitNPCSynergy(Player player, PlayerSynergyItemHandle modplayer, NPC npc, NPC.HitInfo hit, int damageDone)
+        {
+            
+            if (currentState == State.dashing)
+            {
+
+                //deal more damage if dashing
+                int DashBonusDamage = damageDone * 3;
+
+                if(modplayer.StreetLamp_VampireFrogStaff)
+                {
+                    modplayer.StreetLamp_VampireFrogStaff_HitCounter++;
+                    if(modplayer.StreetLamp_VampireFrogStaff_HitCounter >= 3)
+                    {
+                        int SynergyhealAmount = (int)(DashBonusDamage * 0.025f);
+                        player.Heal(SynergyhealAmount);
+                        player.HealEffect(SynergyhealAmount);
+                        modplayer.StreetLamp_VampireFrogStaff_HitCounter = 0;
+
+                    }
+
+
+                }
+                if (modplayer.StreetLamp_Firecracker && Main.rand.NextBool(10))
+                {
+
+                    DashBonusDamage *= 2;
+                    npc.AddBuff(BuffID.OnFire3, 240);
+                }
+
+                float recoil = Main.rand.NextFloat(1, 3);
+                state = State.hit;
+                Projectile.velocity = Projectile.velocity.RotatedBy(MathHelper.ToRadians(Main.rand.Next(160, 200))) * recoil;
+                npc.StrikeNPC(npc.CalculateHitInfo(DashBonusDamage, 1, Main.rand.NextBool(10)));
+            }
+        }
+
+        private void getInfos(Player owner, out Vector2 vectorToIdlePosition, out float distanceToIdlePosition, out bool foundTarget, out float distanceFromTarget, out Vector2 targetCenter)
+        {
+
+            Vector2 idlePosition = owner.Center + new Vector2(0,-80);
+            vectorToIdlePosition = idlePosition - Projectile.Center;
+            distanceToIdlePosition = vectorToIdlePosition.Length();
+
+            if (Main.myPlayer == owner.whoAmI && distanceToIdlePosition > 2000f)
+            {
+                Projectile.position = idlePosition;
+                Projectile.velocity *= 0.1f;
+                Projectile.netUpdate = true;
+            }
+
+            // Starting search distance
+            distanceFromTarget = 350f;
+            targetCenter = owner.Center;
+            foundTarget = false;
+
+            // This code is required if your minion weapon has the targeting feature
+            if (owner.HasMinionAttackTargetNPC)
+            {
+                NPC npc = Main.npc[owner.MinionAttackTargetNPC];
+                float between = Vector2.Distance(npc.Center, owner.Center);
+
+                if (between < 2000f)
+                {
+                    distanceFromTarget = between;
+                    targetCenter = npc.Center;
+                    foundTarget = true;
+                }
+            }
+
+            if (!foundTarget)
+            {
+                for (int i = 0; i < Main.maxNPCs; i++)
+                {
+                    NPC npc = Main.npc[i];
+
+                    if (npc.CanBeChasedBy())
+                    {
+                        float between = Vector2.Distance(npc.Center, owner.Center);
+                        bool closest = Vector2.Distance(Projectile.Center, targetCenter) > between;
+                        bool inRange = between < distanceFromTarget;
+                        
+
+                        if ((closest && inRange || !foundTarget))
+                        {
+                            distanceFromTarget = between;
+                            targetCenter = npc.Center;
+                            foundTarget = true;
+                        }
+                    }
+                }
+            }
+            
+        }
+
+        public void behavior(Player owner, Vector2 vectorToIdlePosition, float distanceToIdlePosition, bool foundTarget, float distanceFromTarget, Vector2 targetCenter, PlayerSynergyItemHandle modplayer)
+        {
+
+            float baseSpeed = 15f;
+            // projectile speed scales with whip attack speed for extra synergy juice
+            float speed = (baseSpeed * owner.GetAttackSpeed(DamageClass.SummonMeleeSpeed)) + (modplayer.StreetLamp_VampireFrogStaff == true ? baseSpeed * 0.15f : 0f);
+            //placeholder
+            Vector2 dashAt = owner.Center;
+            // dashDurtion = distance/speed + baseDashDuration
+            int baseDashDuration = 8;
+            int baseWindUpDuration = 20;
+            float inertia;
+            bool attackable = foundTarget && distanceFromTarget <= 1000f;
+
+            //dash location setup
+            if (currentState != State.dashing && foundTarget)
+            {
+
+                dashAt = targetCenter;
+
+            }
+
+            //reset everything here
+            if (!attackable)
+            {
+                state = State.followOwner;
+                dashDuration = 0;
+                windingUp = 0;
+                attackCooldown = 0;
+
+            }
+
+            //machine state juice
+            switch(currentState, attackable)
+            {
+
+                case (State.followOwner, false):
+
+
+                    Vector2 idlePosition = owner.Center;
+
+                    float minionPositionOffsetX = (10 + Projectile.minionPos * 40) * -owner.direction;
+                    idlePosition.X += minionPositionOffsetX;
+                    float overlapVelocity = 0.4f;
+
+
+                    for (int i = 0; i < Main.maxProjectiles; i++)
+                    {
+                        Projectile other = Main.projectile[i];
+
+                        if (i != Projectile.whoAmI && other.active && other.owner == Projectile.owner && Math.Abs(Projectile.position.X - other.position.X) + Math.Abs(Projectile.position.Y - other.position.Y) < Projectile.width)
+                        {
+                            if (Projectile.position.X < other.position.X)
+                            {
+                                Projectile.velocity.X -= overlapVelocity;
+                            }
+                            else
+                            {
+                                Projectile.velocity.X += overlapVelocity;
+                            }
+
+                            if (Projectile.position.Y < other.position.Y)
+                            {
+                                Projectile.velocity.Y -= overlapVelocity;
+                            }
+                            else
+                            {
+                                Projectile.velocity.Y += overlapVelocity;
+                            }
+                        }
+                    }
+
+                    if (distanceToIdlePosition > 600f)
+                    {
+                        
+                        speed = 12f;
+                        inertia = 10f;
+                    }
+                    else
+                    {
+                        
+                        speed = 4f;
+                        inertia = 20f;
+                    }
+
+                    if (distanceToIdlePosition > 20f)
+                    {
+                        
+                        vectorToIdlePosition.Normalize();
+                        vectorToIdlePosition *= speed;
+                        Projectile.velocity = (Projectile.velocity * (inertia - 1) + vectorToIdlePosition) / inertia;
+                    }
+                    else if (Projectile.velocity == Vector2.Zero)
+                    {
+  
+                        Projectile.velocity.X = -0.15f;
+                        Projectile.velocity.Y = -0.05f;
+                    }
+                    
+                    break;
+
+                //ready, set, GO!
+                case (State.followOwner, true): 
+                    windingUp = baseWindUpDuration;
+                    state = State.windup;
+                    Projectile.velocity.Y = -15;
+                    break;
+
+                case (State.windup, true):
+
+                    if (windingUp > 0)
+                    {
+                        windingUp--;
+                        Projectile.velocity.Y *= 0.90f;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < 15; i++)
+                        {
+
+                            Dust.NewDustPerfect(Projectile.Center, DustID.WhiteTorch, Main.rand.NextVector2CircularEdge(2, 2));
+
+                        }
+                        Projectile.velocity = (((dashAt + new Vector2(Main.rand.Next(-25, 25),Main.rand.Next(-25, 25))) - Projectile.Center).SafeNormalize(Vector2.UnitX) * (speed));
+                        state = State.dashing;
+                        dashDuration = (int)((Projectile.Center.Distance(dashAt))/(speed)) + baseDashDuration;
+                        Projectile.ResetLocalNPCHitImmunity();
+                    }
+
+
+                    break;
+
+                case (State.dashing, true):
+                   
+                    dashDuration--;
+                    if(dashDuration <= 0)
+                    {
+                        attackCooldown = 20;
+                        state = State.miss;
+                    }
+                    break;
+                
+
+                case(State.hit, false || true):
+
+                    dashDuration = 0;
+                    Projectile.velocity *= 0.75f;
+                    attackCooldown--;
+                    if (attackCooldown <= 0)
+                    {
+                        Projectile.velocity *= 0;
+                        state = State.followOwner;
+                    }
+
+                    break;
+
+                case (State.miss, true || false):
+                    state = State.followOwner;
+                    Projectile.velocity *= 0;
+                    break;
+            }
+        }
+
+        //renames for easier readabilty
+        public int windingUp { 
+            
+            
+            get => (int)Projectile.ai[0];
+            set => Projectile.ai[0] = value; 
+
+        }
+
+        public int dashDuration
+        {
+
+
+            get => (int)Projectile.ai[1];
+            set => Projectile.ai[1] = value;
+
+        }
+
+        public int attackCooldown
+        {
+
+
+            get => (int)Projectile.ai[2];
+            set => Projectile.ai[2] = value;
+
+        }
+
+        public enum State
+        {
+
+            //always windup before dashing 
+            windup,
+            dashing,
+
+            miss,
+            hit,
+
+            followOwner
+
+        }
+
+        //change states with this
+        private State state = State.followOwner;
+
+        //ready only
+        public State currentState { get { return state; } }
+
+        private bool CheckActive(Player owner)
+        {
+            if (owner.dead || !owner.active)
+            {
+                owner.ClearBuff(ModContent.BuffType<MothBuff>());
+
+                return false;
+            }
+
+            if (owner.HasBuff(ModContent.BuffType<MothBuff>()))
+            {
+                Projectile.timeLeft = 2;
+            }
+            return true;
+        }
+
+        public void SelectFrame()
+        {
+            if (++Projectile.frameCounter >= 6)
+            {
+                Projectile.frameCounter = 0;
+                Projectile.frame += 1;
+                if (Projectile.frame >= Main.projFrames[Projectile.type])
+                {
+                    Projectile.frame = 0;
+                }
+            }
+        }
+
+
+
+    }
+}
+
+
+  
