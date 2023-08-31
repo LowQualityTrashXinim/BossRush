c941b5d4d71e748361f407bbf28bf9173ba398a6
Xinim <LELwhyyoulookingatthis@gmail.com>
2023-08-31 17:00:29 +0700

    Update UI code to be a lot more clean

--- Contents/Projectiles/TinProjectile.cs
+++ Contents/Projectiles/TinProjectile.cs
@@ -1,10 +1,9 @@
-﻿using Terraria;
+﻿using System;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
-using BossRush.Contents.Items.Weapon;
 using Microsoft.Xna.Framework.Graphics;
-using System;
 
 namespace BossRush.Contents.Projectiles
 {
