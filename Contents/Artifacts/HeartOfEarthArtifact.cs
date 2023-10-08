9bb403c894d4e2bd463c7035e4f6d796ebc05a8e
sucs28mee <adamdudkiewicz2003@gmail.com>
2023-10-08 19:54:44 +0200

    Added back the artifacts cuz they were ignored for some reason

---
+++ Contents/Artifacts/HeartOfEarthArtifact.cs
@@ -0,0 +1,83 @@
+﻿using BossRush.Common.Systems.ArtifactSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace BossRush.Contents.Artifacts
+{
+    internal class HeartOfEarthArtifact : Artifact
+    {
+        public override float Scale => 0.7f;
+    }
+
+    class HeartOfEarthPlayer : ModPlayer
+    {
+        bool Earth = false;
+        public override void ResetEffects()
+        {
+            Earth = Player.ActiveArtifact() == Artifact.ArtifactType<HeartOfEarthArtifact>();
+        }
+
+        int EarthCD = 0;
+        int ShortStanding = 0;
+        public override void ModifyMaxStats(out StatModifier health, out StatModifier mana)
+        {
+            base.ModifyMaxStats(out health, out mana);
+            if (Earth)
+            {
+                health.Base = 100 + Player.statLifeMax * 1.5f;
+            }
+        }
+        public override void PostUpdate()
+        {
+            if (Player.velocity == Vector2.Zero)
+            {
+                ShortStanding++;
+                if (ShortStanding > 120)
+                {
+                    if (ShortStanding % Math.Clamp((10 - ShortStanding / 100), 1, 10) == 0)
+                    {
+                        Player.statLife = Math.Clamp(Player.statLife + 1, 0, Player.statLifeMax2);
+                    }
+                }
+            }
+            else
+            {
+                ShortStanding = 0;
+            }
+            if (Earth)
+            {
+                EarthCD = BossRushUtils.CoolDown(EarthCD);
+                if (EarthCD > 0)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        int dust = Dust.NewDust(Player.Center + Main.rand.NextVector2Circular(10, 30), 0, 0, DustID.Blood);
+                        Main.dust[dust].velocity = -Vector2.UnitX * Player.direction * 2f;
+                    }
+                }
+            }
+        }
+        public override bool CanUseItem(Item item)
+        {
+            if (Earth)
+            {
+                return EarthCD <= 0;
+            }
+            return base.CanUseItem(item);
+        }
+        public override void OnHurt(Player.HurtInfo info)
+        {
+            if (Earth)
+            {
+                EarthCD = 300;
+            }
+        }
+    }
+}
---
+++ Contents/Artifacts/TokenOfPrideArtifact.cs
@@ -0,0 +1,42 @@
+﻿using BossRush.Contents.Items.Chest;
+using BossRush.Contents.Items.NohitReward;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using BossRush.Common.Systems.ArtifactSystem;
+
+namespace BossRush.Contents.Artifacts
+{
+    internal class TokenOfPrideArtifact : Artifact
+    {
+        public override int Frames => 10;
+    }
+
+    public class PridePlayer : ModPlayer
+    {
+        bool Pride = false; protected ChestLootDropPlayer chestmodplayer => Player.GetModPlayer<ChestLootDropPlayer>();
+        public override void ResetEffects()
+        {
+            Pride = Player.HasArtifact<TokenOfPrideArtifact>();
+        }
+        public override void PostUpdate()
+        {
+            if (Pride)
+            {
+                chestmodplayer.finalMultiplier -= .5f;
+            }
+        }
+        public override void ModifyWeaponDamage(Item item, ref StatModifier damage)
+        {
+            if (Pride)
+            {
+                float reward = Player.GetModPlayer<NoHitPlayerHandle>().BossNoHitNumber.Count * .1f;
+                damage += .45f + reward;
+            }
+        }
+    }
+}
