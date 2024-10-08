c099e9baa0f6a612c1ba9c5dc5b0fe8bef453594
LowQualityTrashXinim <LELwhyyoulookingatthis@gmail.com>
2024-10-08 21:42:31 +0700

    remove debug text

--- Common/Nightmare/NightmarePlayer.cs
+++ Common/Nightmare/NightmarePlayer.cs
@@ -1,11 +1,16 @@
 ﻿using BossRush.Common.General;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace BossRush.Common.Nightmare {
 	internal class NightmarePlayer : ModPlayer {
 		public override void OnEnterWorld() {
-			if (NightmareSystem.IsANightmareWorld())
-				Player.difficulty = 2;
+			//if (NightmareSystem.IsANightmareWorld()) {
+			//	Main.NewText("Nightmare : True", color: Microsoft.Xna.Framework.Color.Red);
+			//}
+			//else {
+			//	Main.NewText("Nightmare : False", color: Microsoft.Xna.Framework.Color.Green);
+			//}
 		}
 	}
 }
