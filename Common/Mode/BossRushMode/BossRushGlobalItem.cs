ac5be1e263f8d2892ab857dca593c1cd1ba44935
rakan <alhrbidotcom@gmail.com>
2024-10-22 17:18:16 +0300

    night bosses summons now resets to night regardless if its day or not + added missing summon items

--- Common/Mode/BossRushMode/BossRushGlobalItem.cs
+++ Common/Mode/BossRushMode/BossRushGlobalItem.cs
@@ -9,6 +9,7 @@ using BossRush.Contents.Items.Consumable.Spawner;
 namespace BossRush.Common.Mode.BossRushMode;
 internal class BossRushGlobalItem : GlobalItem {
 	public override bool CanUseItem(Item item, Player player) {
+
 		if (!UniversalSystem.CanAccessContent(player, UniversalSystem.BOSSRUSH_MODE)) {
 			return base.CanUseItem(item, player);
 		}
@@ -17,23 +18,38 @@ internal class BossRushGlobalItem : GlobalItem {
 		}
 		if (item.type == ItemID.SuspiciousLookingEye) {
 			bool checkThisFirst = BossRushWorldGen.IsInBiome(player, BiomeAreaID.FleshRealm, ModContent.GetInstance<BossRushWorldGen>().Room);
-			if (Main.dayTime && checkThisFirst) {
-				Main.time = Main.dayLength;
+			if (Main.netMode != NetmodeID.MultiplayerClient && checkThisFirst) {
+				Main.SkipToTime(0, false);
 			}
 			return checkThisFirst;
 		}
+		if (item.type == ItemID.EmpressButterfly || item.type == ItemID.MechanicalEye || item.type == ItemID.MechanicalSkull || item.type == ItemID.MechanicalWorm) {
+			bool checkThisFirst = BossRushWorldGen.IsInBiome(player, BiomeAreaID.Hallow, ModContent.GetInstance<BossRushWorldGen>().Room);
+			if (Main.netMode != NetmodeID.MultiplayerClient && checkThisFirst) {
+				Main.SkipToTime(0, false);
+			}
+			return checkThisFirst;
+		}
+		if (item.type == ModContent.ItemType<PlanteraSpawn>() || item.type == ItemID.LihzahrdPowerCell) {
+
+			return BossRushWorldGen.IsInBiome(player, BiomeAreaID.Jungle, ModContent.GetInstance<BossRushWorldGen>().Room);
+		}
+
 		if (item.type == ItemID.WormFood) {
 			return BossRushWorldGen.IsInBiome(player, BiomeAreaID.Corruption, ModContent.GetInstance<BossRushWorldGen>().Room);
 		}
 		if (item.type == ItemID.BloodySpine) {
 			return BossRushWorldGen.IsInBiome(player, BiomeAreaID.Crimson, ModContent.GetInstance<BossRushWorldGen>().Room);
 		}
-		if (item.type == ModContent.ItemType<CursedDoll>()) {
+		if (item.type == ModContent.ItemType<CursedDoll>() || item.type == ModContent.ItemType<LunaticTablet>()) {
 			bool checkThisFirst = BossRushWorldGen.IsInBiome(player, BiomeAreaID.Dungeon, ModContent.GetInstance<BossRushWorldGen>().Room);
-			if (Main.dayTime && checkThisFirst) {
-				Main.time = Main.dayLength;
+			if (Main.netMode != NetmodeID.MultiplayerClient && checkThisFirst) {
+				Main.SkipToTime(0, false);
 			}
 			return BossRushWorldGen.IsInBiome(player, BiomeAreaID.Dungeon, ModContent.GetInstance<BossRushWorldGen>().Room);
+		}
+		if (item.type == ItemID.TruffleWorm) {
+			return BossRushWorldGen.IsInBiome(player, BiomeAreaID.BeeNest, ModContent.GetInstance<BossRushWorldGen>().Room);
 		}
 		if (item.type == ItemID.Abeemination) {
 			return BossRushWorldGen.IsInBiome(player, BiomeAreaID.BeeNest, ModContent.GetInstance<BossRushWorldGen>().Room);
