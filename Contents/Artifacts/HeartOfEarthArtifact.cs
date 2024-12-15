ca95f831330f641f4656193be1ed36d728fe2db9
LowQualityTrashXinim <LELwhyyoulookingatthis@gmail.com>
2024-12-15 21:01:41 +0700

    Added perk for Heart of Earth

--- Contents/Artifacts/HeartOfEarthArtifact.cs
+++ Contents/Artifacts/HeartOfEarthArtifact.cs
@@ -3,6 +3,10 @@ using Terraria;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
 using BossRush.Common.Systems.ArtifactSystem;
+using BossRush.Contents.Perks;
+using BossRush.Common.Systems.Achievement;
+using BossRush.Common.Systems;
+using System.Collections.Generic;
 
 namespace BossRush.Contents.Artifacts {
 	internal class HeartOfEarthArtifact : Artifact {
@@ -46,7 +50,7 @@ namespace BossRush.Contents.Artifacts {
 			}
 		}
 		public override void OnHitByNPC(NPC npc, Player.HurtInfo hurtInfo) {
-			if(!Earth) {
+			if (!Earth) {
 				return;
 			}
 			ShortStanding = (int)(ShortStanding * .75f);
@@ -58,6 +62,52 @@ namespace BossRush.Contents.Artifacts {
 			}
 			ShortStanding = (int)(ShortStanding * .75f);
 			OnHitDelay = BossRushUtils.ToSecond(.5f);
+		}
+	}
+
+	public class EarthlyWarm : Perk {
+		public override void SetDefaults() {
+			CanBeStack = true;
+			StackLimit = 3;
+		}
+		public override bool SelectChoosing() {
+			return Artifact.PlayerCurrentArtifact<HeartOfEarthArtifact>() || AchievementSystem.IsAchieved("HeartOfEarth");
+		}
+		public override void UpdateEquip(Player player) {
+			PlayerStatsHandle.AddStatsToPlayer(player, PlayerStats.RegenHP, Additive: 1f + 0.5f * StackAmount(player));
+		}
+		public override void OnHitByAnything(Player player) {
+			if (Main.rand.NextBool(Main.rand.Next(3, Math.Clamp(11 - StackAmount(player), 4, 10)))) {
+				player.Heal(Main.rand.Next((int)(player.statLife / (Math.Clamp(2f - .1f * StackAmount(player), .1f, 9)))));
+			}
+			if (Main.rand.NextBool(200)) {
+				player.Heal(player.statLifeMax2);
+			}
+		}
+	}
+	public class NaturalThorn : Perk {
+		public override void SetDefaults() {
+			CanBeStack = true;
+			StackLimit = 3;
+		}
+		public override bool SelectChoosing() {
+			return Artifact.PlayerCurrentArtifact<HeartOfEarthArtifact>() || AchievementSystem.IsAchieved("HeartOfEarth");
+		}
+		public override void UpdateEquip(Player player) {
+			int stack = StackAmount(player);
+			PlayerStatsHandle.AddStatsToPlayer(player, PlayerStats.MaxHP, 1 + .1f * stack);
+			player.Center.LookForHostileNPC(out List<NPC> npclist, 200);
+			foreach (NPC npc in npclist) {
+				if (npc.immune[player.whoAmI] <= 0)
+					player.StrikeNPCDirect(npc, npc.CalculateHitInfo((player.statLifeMax2 - player.statLife) * stack, 1));
+			}
+		}
+		public override void OnHitByNPC(Player player, NPC npc, Player.HurtInfo hurtInfo) {
+			NPC.HitInfo hit = new();
+			hit.Damage = (int)player.GetModPlayer<PlayerStatsHandle>().UpdateHPRegen.ApplyTo(hurtInfo.Damage * .25f * StackAmount(player)) + player.lifeRegen;
+			hit.Knockback = 10;
+			hit.HitDirection = BossRushUtils.DirectionFromPlayerToNPC(player.Center.X, npc.Center.X);
+			player.StrikeNPCDirect(npc, hit);
 		}
 	}
 }
