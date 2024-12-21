c6b6b42b48ef4535fd3354f27928e3b837a8b251
LowQualityTrashXinim <LELwhyyoulookingatthis@gmail.com>
2024-12-22 02:33:29 +0700

    Update sprite

--- Contents/Items/Accessories/SynergyAccessories/ArcaneMaster.cs
+++ Contents/Items/Accessories/SynergyAccessories/ArcaneMaster.cs
@@ -10,7 +10,6 @@ using BossRush.Contents.Items.Weapon;
 
 namespace BossRush.Contents.Items.Accessories.SynergyAccessories;
 internal class ArcaneMaster : SynergyModItem {
-	public override string Texture => BossRushTexture.MissingTexture_Default;
 	public override void SetDefaults() {
 		Item.DefaultToAccessory(30, 30);
 		Item.value = 1000000;
