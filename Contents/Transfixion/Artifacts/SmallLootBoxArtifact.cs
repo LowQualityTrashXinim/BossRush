1825c0429e4bceea1f7904adbc1a266138400fa9
LowQualityTrashXinim <LELwhyyoulookingatthis@gmail.com>
2025-05-01 22:40:00 +0700

    added small lootbox artifact

---
+++ Contents/Transfixion/Artifacts/SmallLootBoxArtifact.cs
@@ -0,0 +1,11 @@
+﻿using BossRush.Common.Systems.ArtifactSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BossRush.Contents.Transfixion.Artifacts;
+
+public class SmallLootBoxArtifact : Artifact {
+}
